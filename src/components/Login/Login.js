a2da4dda1cc6d8df2ffd67f77a1c6dc35c427323
buradabhargavi <bhargaviboorada246@gmail.com>
2024-05-03 16:50:04 +0530

    Initial commit

---
+++ src/components/Login/Login.js
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from "react";
+import Card from "../UI/Card/Card";
+import classes from "./Login.module.css";
+import Button from "../UI/Button/Button";
+
+const Login = (props) => {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [emailIsValid, setEmailIsValid] = useState();
+  const [enteredPassword, setEnteredPassword] = useState("");
+  const [passwordIsValid, setPasswordIsValid] = useState();
+  const [enteredCollege, setEnteredCollege] = useState("");
+  const [collegeIsValid, setCollegeIsValid] = useState();
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  useEffect(() => {
+    setFormIsValid(emailIsValid && passwordIsValid && collegeIsValid);
+  }, [emailIsValid, passwordIsValid, collegeIsValid]);
+
+  const emailChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+    setEmailIsValid(event.target.value.includes("@"));
+  };
+
+  const passwordChangeHandler = (event) => {
+    setEnteredPassword(event.target.value);
+    setPasswordIsValid(event.target.value.trim().length > 6);
+  };
+
+  const collegeChangeHandler = (event) => {
+    setEnteredCollege(event.target.value);
+    setCollegeIsValid(event.target.value.trim().length > 0); // Adjust this validation according to your requirements
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    props.onLogin(enteredEmail, enteredPassword, enteredCollege);
+  };
+
+  return (
+    <Card className={classes.login}>
+      <form onSubmit={submitHandler}>
+        <div
+          className={`${classes.control} ${
+            emailIsValid === false ? classes.invalid : ""
+          }`}
+        >
+          <label htmlFor="email">E-Mail</label>
+          <input
+            type="email"
+            id="email"
+            value={enteredEmail}
+            onChange={emailChangeHandler}
+          />
+        </div>
+        <div
+          className={`${classes.control} ${
+            passwordIsValid === false ? classes.invalid : ""
+          }`}
+        >
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={enteredPassword}
+            onChange={passwordChangeHandler}
+          />
+        </div>
+
+        <div
+          className={`${classes.control} ${
+            collegeIsValid === false ? classes.invalid : ""
+          }`}
+        >
+          <label htmlFor="college">College</label>
+          <input
+            type="text"
+            id="college"
+            value={enteredCollege}
+            onChange={collegeChangeHandler}
+          />
+        </div>
+        <div className={classes.actions}>
+          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+            Login
+          </Button>
+        </div>
+      </form>
+    </Card>
+  );
+};
+
+export default Login;
